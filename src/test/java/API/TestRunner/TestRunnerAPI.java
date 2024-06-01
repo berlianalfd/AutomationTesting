b47599694b69c48add6bc8eeeea1bcd3fbb7a01e
Berliana Elfada <berlianaelfd23@gmail.com>
2024-06-01 23:49:40 +0700

    update test runner

--- src/test/java/API/TestRunner/TestRunnerAPI.java
+++ src/test/java/API/TestRunner/TestRunnerAPI.java
@@ -1,14 +1,14 @@
 package API.TestRunner;
 
+import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features="src/test/resources/features/API",
-        glue= {"stepDefinitions"},
-        plugin ={"pretty","json:target/cucumber-reports/APIReport.json"}
-        )
+    features = "src/test/resources/features/API",
+    glue = {"API.stepDefinitions"},
+    plugin = {"pretty", "html:target/cucumber-reports.html"}
+)
 public class TestRunnerAPI {
 }
--- src/test/java/WEB/TestRunner/TestRunnerWEB.java
+++ src/test/java/WEB/TestRunner/TestRunnerWEB.java
@@ -1,14 +1,14 @@
 package WEB.TestRunner;
 
+import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features="src/test/resources/features/WEB",
-        glue= {"stepDefinitions"},
-        plugin ={"pretty","json:target/cucumber-reports/WEBReport.json"}
-        )
+    features = "src/test/resources/features/WEB",
+    glue = {"WEB.stepDefinitions"},
+    plugin = {"pretty", "html:target/cucumber-reports.html"}
+)
 public class TestRunnerWEB {
 }
