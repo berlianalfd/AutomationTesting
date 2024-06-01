b21e8ca726c9fe074839248ae5c96dd4f9af59a9
Berliana Elfada <berlianaelfd23@gmail.com>
2024-06-02 00:18:07 +0700

    update test generate report

--- src/test/java/WEB/TestRunner/TestRunnerWEB.java
+++ src/test/java/WEB/TestRunner/TestRunnerWEB.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
     features = "src/test/resources/features/WEB",
     glue = {"WEB.stepDefinitions"},
-    plugin = {"pretty", "html:target/cucumber-reports.html"}
+    plugin = {"pretty", "html:target/cucumber-report-html/cucumber-reports-web.html"}
 )
 public class TestRunnerWEB {
 }
